dde42fe0e27ea0c4a103441d6bba075802df148d
Hankypoo7 <100436822+hankypoo7@users.noreply.github.com>
2024-12-18 12:14:36 -0600

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,79 +1,66 @@
 let modInfo = {
-	name: "The YouTube Tree",
-	author: "Henry Wemmie",
-	pointsName: "Views",
-	modFiles: ["layers.js", "tree.js"],
+    name: "The YouTube Tree",
+    author: "Henry Wemmie",
+    pointsName: "Views & Money",
+    modFiles: ["layers.js", "tree.js"],
 
-	discordName: "",
-	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
-	offlineLimit: 1,  // In hours
+    discordName: "",
+    discordLink: "",
+    initialStartPoints: new Decimal(10),
+    offlineLimit: 1,  // In hours
 }
 
-// Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+    num: "0.0",
+    name: "Literally nothing",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+    <h3>v0.0</h3><br>
+        - Added things.<br>
+        - Added stuff.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
-// If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
-// (The ones here are examples, all official functions are already taken care of)
 var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
 
-function getStartPoints(){
+function getStartPoints() {
     return new Decimal(modInfo.initialStartPoints)
 }
 
-// Determines if it should show points/sec
-function canGenPoints(){
-	return true
+function canGenPoints() {
+    return true
 }
 
-// Calculate points/sec!
 function getPointGen() {
-	if(!canGenPoints())
-		return new Decimal(0)
-	let gain = new Decimal(1)
-	if (hasUpgrade('p', 11)) gain = gain.times(2)
-	if (hasUpgrade('p', 12)) gain = gain.times(upgradeEffect('p', 12))
-	return gain
+    if (!canGenPoints())
+        return new Decimal(0);
+    let gain = new Decimal(1);
+    if (hasUpgrade('p', 11)) gain = gain.times(2);
+    if (hasUpgrade('p', 12)) gain = gain.times(upgradeEffect('p', 12));
+    if (hasUpgrade('m', 11)) gain = gain.times(upgradeEffect('m', 11)); // Money upgrades
+    return gain;
 }
 
-// You can add non-layer related variables that should to into "player" and be saved here, along with default values
-function addedPlayerData() { return {
-}}
+function addedPlayerData() {
+    return {
+        money: new Decimal(0), // Starting money
+    };
+}
 
-// Display extra things at the top of the page
 var displayThings = [
-]
+    "You are gaining " + format(getPointGen()) + " views per second.",
+    "You have " + format(player["m"].points) + " money.", // Display money
+];
 
-// Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+    return player.points.gte(new Decimal("e280000000")) && player["m"].points.gte(new Decimal(1e6));
 }
 
+var backgroundStyle = {}
 
-
-// Less important things beyond this point!
-
-// Style for the background, can be a function
-var backgroundStyle = {
-
-}
-
-// You can change this if you have things that can be messed up by long tick lengths
 function maxTickLength() {
-	return(3600) // Default is 1 hour which is just arbitrarily large
+    return(3600) // Default is 1 hour
 }
 
-// Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
-// you can cap their current resources with this.
-function fixOldSave(oldVersion){
-}
+function fixOldSave(oldVersion) {}
