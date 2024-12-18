68970ff031e0ca68122096f43a71d8bddbfe0a7a
Hankypoo7 <100436822+hankypoo7@users.noreply.github.com>
2024-12-18 08:41:07 -0600

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	author: "nobody",
-	pointsName: "points",
+	name: "The YouTube Tree",
+	author: "Henry Wemmie",
+	pointsName: "Views",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
