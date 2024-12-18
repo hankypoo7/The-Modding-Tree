855a2ab0e4437ed6da448dc60e82d2c4c6b7d3c6
Hankypoo7 <100436822+hankypoo7@users.noreply.github.com>
2024-12-18 09:05:16 -0600

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -2,19 +2,21 @@ addLayer("p", {
     name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
-    }},
+    startData() { 
+        return {
+            unlocked: true,
+            points: new Decimal(0),
+        }
+    },
     color: "#ff0400",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "Subscribers", // Name of prestige currency
     baseResource: "Views", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    baseAmount() { return player.points }, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
+        let mult = new Decimal(1)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -24,12 +26,12 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for Subscribers", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true}, // <-- Missing comma added here
     upgrades: {
         11: {
-    		title: "First Sub!",
-   		description: "Double your views gain",
-    		cost: new Decimal(1),
+            title: "First Sub!",
+            description: "Double your views gain",
+            cost: new Decimal(1),
         },
     },
 })
